69cb9d11f3b9b7a2bd79949d5a4139369f5ecb7f
cout-raj-explorer <aryanrinchu@gmail.com>
2025-08-06 21:16:43 +0530

    updated state

--- State/state.py
+++ State/state.py
@@ -1,11 +1,11 @@
-from typing import TypedDict, List, Annotated
-
+from typing import TypedDict, List, Annotated, Optional
+    
 class BaseMessages(TypedDict):
-    messages : List = []
-    history : List = []
-    video_path : str = None
-    response : List = []
-    video_context : List = []
-    need_summarizer : int 
-    events : List= []
-    need_events : int
+    messages: Annotated[List[str], "List of message strings"]
+    history: Annotated[List[str], "List of history strings"]
+    video_path: Annotated[Optional[str], "Path to the video, or None"]
+    response: Annotated[List[str], "List of response strings"]
+    video_context: Annotated[List[str], "Contextual information from video"]
+    need_summarizer: Annotated[int, "Flag to indicate summarizer needed"]
+    events: Annotated[List[str], "List of events"]
+    need_events: Annotated[int, "Flag to indicate events needed"]
